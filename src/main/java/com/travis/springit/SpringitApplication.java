e3eaf95c5da054aaaea2246e2f18b29c581facf1
jeremite <543084991@qq.com>
2020-12-21 12:08:19 -0800

    added account register adn profile mappings and templates

--- src/main/java/com/travis/springit/SpringitApplication.java
+++ src/main/java/com/travis/springit/SpringitApplication.java
@@ -10,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
 //@EnableJpaAuditing
@@ -27,7 +28,12 @@ public class SpringitApplication {
 	PrettyTime prettyTime(){
 		return new PrettyTime();
 	}
-
+	//TODO * Configuring this bean should not be needed once Spring Boot's Thymeleaf starter includes configuration
+	//TODO   for thymeleaf-extras-springsecurity5 (instead of thymeleaf-extras-springsecurity4)
+	@Bean
+	public SpringSecurityDialect securityDialect() {
+		return new SpringSecurityDialect();
+	}
 
 
 }
--- src/main/java/com/travis/springit/controller/AutoController.java
+++ src/main/java/com/travis/springit/controller/AutoController.java
@@ -9,4 +9,13 @@ public class AutoController {
     public String login(){
         return "auth/login";
 }
+    @GetMapping("/profile")
+    public String profile() {
+        return "auth/profile";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "auth/register";
+    }
 }
